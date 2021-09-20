f9849de4352ad68ddb382afeeedc192f4756a888
Peter Gakure <petergakure97@gmail.com>
2021-09-21 00:05:12 +0300

    update modified file

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,46 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
+from config import config_options
 
-    # from config import Config
-app = Flask(__name__)
-
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-bootstrap = Bootstrap(app)
-mail = Mail(app)
-login_manager.login_view = 'auth.login'
+login_manager = LoginManager()
 login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
 
 
-# def create_app(config_name):
-#     app.config.from_object(config_options[config_name])
-#     from .auth import auth as auth_blueprint
-#     from .main import main as main_blueprint
-#     app.register_blueprint(auth_blueprint)
-#     app.register_blueprint(main_blueprint)
-#     return app
+def create_app(config_name):
+    app = Flask(__name__)
+
+
+
+    # Creating the app configurations
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://peter:peter2021@localhost/PitchApp'
+
+    # Creating the app configurations
+
+    app.config.from_object(config_options[config_name])
+    # config_options[config_name].init_app(app)
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    
+    return app
