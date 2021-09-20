8fe4622b93774ba4fd323fa0b988e3a4a2828a6f
Peter Gakure <petergakure97@gmail.com>
2021-09-20 18:12:29 +0300

    Update modified file

--- app/email.py
+++ app/email.py
@@ -2,7 +2,7 @@ from flask_mail import Message
 from flask import render_template
 from . import mail
 
-def mail_message(subject,template,to,**kwargs):
+def email_message(subject,template,to,**kwargs):
     sender_email = 'p@gmail.com'
 
     email = Message(subject, sender=sender_email, recipients=[to])
