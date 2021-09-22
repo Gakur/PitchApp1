00fe42d9a8a996cecaab5692b5a5f0dc0176b8a7
Peter Gakure <petergakure97@gmail.com>
2021-09-22 12:38:07 +0300

    Update modified file

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 export MAIL_USERNAME='p@gmail.com'
-export MAIL_PASSWORD=''
+export MAIL_PASSWORD='peter123123'
 export SECRET_KEY='secret'
+# export DATABASE_URL="postgresql:///pitchapp1"
 
 python3.8 manage.py server
